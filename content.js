ab6de61e70409959290fe0b5b35b8d3642ad2a7e
Nicolas Cousin <35494081+nicocouz@users.noreply.github.com>
2022-04-28 15:40:16 +0200

    Update content.js

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-// Definition of mutationObserver constructor
+// Definition of mutationObserver instance
 const mutationObserver = new MutationObserver(entries => {
 // Get the innerHTML element that will be modify during your DOM modification
     var navbarinnerHTML = document.querySelector("#profile_legend_account").innerHTML
@@ -30,14 +30,14 @@ window.addEventListener("load", function () {
                 document.querySelector("#mainHeader > div").style.background = ""
             )
             const childNavbar = document.querySelector("#profile_menu_button > div.productProfile_body")
-            // Use of mutationObserver constructor
+            // Use of mutationObserver instance
             mutationObserver.observe(childNavbar, { childList: true, subtree: true })
         }, 4500);
     } else {
         // FOR CLIENTSIDE
 
         const childNavbar = document.querySelector("#profile_menu_button > div.productProfile_body")
-                    // Use of mutationObserver constructor
+        // Use of mutationObserver instance
         mutationObserver.observe(childNavbar, { childList: true, subtree: true })
     }
 });
