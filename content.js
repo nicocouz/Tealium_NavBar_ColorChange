e530d034b8ea378c2f448e60a30ccb1c01264766
Nicolas Cousin <35494081+nicocouz@users.noreply.github.com>
2022-04-28 15:38:11 +0200

    Update content.js
    comments added

--- content.js
+++ content.js
@@ -1,19 +1,23 @@
 
 const mutationObserver = new MutationObserver(entries => {
-
+// Get the innerHTML element that will be modify during your DOM modification
     var navbarinnerHTML = document.querySelector("#profile_legend_account").innerHTML
     if (navbarinnerHTML === "caisse-epargne") {
+     // Change the background color if caisse-epargne
         document.querySelector("#main_navigation > div.nui-main-header > div").style.background = "#e10808"
     }
     else (
+     // Keep the default background if else
         document.querySelector("#main_navigation > div.nui-main-header > div").style.background = ""
     )
+
     if (navbarinnerHTML === "caisse-epargne" && document.querySelector("#mainHeader > div") != null) {
         document.querySelector("#mainHeader > div").style.background = "#e10808"
     }
 
 })
 
+// Wait until window is loaded
 window.addEventListener("load", function () {
     // FOR SERVERSIDE
     if (window.name === 'datacloud') {
